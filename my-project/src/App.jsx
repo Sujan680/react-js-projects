182da84a7da05b855e4700daa8810e54a6d30e15
sujan <budhasujan3@gmail.com>
2024-07-31 11:30:53 +0545

    Multiple Selection Accordian using react js

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,9 +1,11 @@
 import Accordian from "./components/accordian"
+import MultiSelectionAccordian from "./components/accordian/MultipleSelection"
 
 const App = () => {
   return (
     <div>
       <Accordian/>
+      <MultiSelectionAccordian/>
     </div>
   )
 }
