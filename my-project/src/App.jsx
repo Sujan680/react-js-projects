28e80b3ac3e28fad1285f02c8523bbb03d06794c
sujan <budhasujan3@gmail.com>
2024-08-01 18:58:01 +0545

    Star Rating in react js

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,6 +2,7 @@ import Accordian from "./components/accordian"
 import MultiSelectionAccordian from "./components/accordian/MultipleSelection"
 import RandomColor from "./components/random-color"
 import BgChanger from "./components/random-color/BgChanger"
+import StarRating from "./components/star-rating"
 
 const App = () => {
   return (
@@ -10,7 +11,9 @@ const App = () => {
       <MultiSelectionAccordian/> */}
 
       {/* <RandomColor/> */}
-      <BgChanger/>
+      {/* <BgChanger/> */}
+
+      <StarRating noOfStars={10}/>
 
     </div>
   )
